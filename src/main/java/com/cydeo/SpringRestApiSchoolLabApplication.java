ca6a31a9456bc7085452a4e932c8dff39f3c6362
nijat <nijat.de@gmail.com>
2023-01-02 16:18:46 +0100

    Spring rest api created

---
+++ src/main/java/com/cydeo/SpringRestApiSchoolLabApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiSchoolLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApiSchoolLabApplication.class, args);
+    }
+
+}
