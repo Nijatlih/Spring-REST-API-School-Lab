8efef0146737969a2a34d0353c52e6c3baa63dae
nijat <nijat.de@gmail.com>
2023-01-07 11:18:20 +0100

    Spring rest api controller

---
+++ src/main/java/com/cydeo/controller/ApiController.java
@@ -0,0 +1,71 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.AddressDTO;
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.AddressService;
+import com.cydeo.service.ParentService;
+import com.cydeo.service.StudentService;
+import com.cydeo.service.TeacherService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ApiController {
+
+    private final TeacherService teacherService;
+    private final StudentService studentService;
+    private final ParentService parentService;
+    private final AddressService addressService;
+
+    public ApiController(TeacherService teacherService, StudentService studentService,
+                         ParentService parentService, AddressService addressService) {
+        this.teacherService = teacherService;
+        this.studentService = studentService;
+        this.parentService = parentService;
+        this.addressService = addressService;
+    }
+
+    @GetMapping("/teachers")
+    public List<TeacherDTO> readAllTeachers() {
+        return teacherService.findAll();
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<ResponseWrapper> readAllStudents() {
+        return ResponseEntity.ok(new ResponseWrapper("Students are successfully retrieved.", studentService.findAll()));
+    }
+
+    @GetMapping("/parents")
+    public ResponseEntity<ResponseWrapper> readAllParents() {
+        ResponseWrapper responseWrapper =
+                new ResponseWrapper(true, "Parents are successfully retrieved.", HttpStatus.OK.value(), parentService.findAll());
+        return ResponseEntity.status(HttpStatus.OK).body(responseWrapper);
+    }
+
+    @GetMapping("/address/{id}")
+    public ResponseEntity<ResponseWrapper> getAddress(@PathVariable("id") Long id) throws Exception {
+        AddressDTO addressToReturn = addressService.findById(id);
+        addressToReturn.setCurrentTemperature(addressService.getCurrentWeather(addressToReturn.getCity()).getCurrent().getTemperature());
+
+        return ResponseEntity.ok(new ResponseWrapper("Address iss successfully retrieved.", addressToReturn));
+    }
+
+    @PutMapping("/address/{id}")
+    public AddressDTO updateAddress(@PathVariable("id") Long id,
+                                    @RequestBody AddressDTO addressDTO) throws Exception {
+
+        addressDTO.setId(id);
+
+        AddressDTO addressToReturn = addressService.update(addressDTO);
+
+        addressToReturn.setCurrentTemperature(addressService.getCurrentWeather(addressToReturn.getCity()).getCurrent().getTemperature());
+
+        return addressToReturn;
+
+    }
+
+}
